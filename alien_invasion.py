e33fb9a0bcd0cc3133aa682e1f33b83afc1b2ce9
Kasia Romaniuk <k.romaniuk99@gmail.com>
2020-04-09 21:10:05 +0200

    Added a settings class storing this game's settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,21 +2,26 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
-    #Game init plus object creation
+    # Game init plus object creation
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_height, ai_settings.screen_width))
     pygame.display.set_caption("Alien invasion")
 
-    #Begin game's main loop
+    # Begin game's main loop
     while True:
 
-        #Awaiting keyboard or mouse input
+        # Awaiting keyboard or mouse input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        #Display last modified screen
+        screen.fill(ai_settings.bg_color)
+
+        # Display last modified screen
         pygame.display.flip()
 
 run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    ''' Class that stores all settings realated to this game '''
+
+    def __init__(self):
+        ''' Init of game settings '''
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
