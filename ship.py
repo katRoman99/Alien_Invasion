3c6a24d6dabd4c25a61baefc05bdfb6a92a4f911
Kasia Romaniuk <k.romaniuk99@gmail.com>
2020-04-10 18:27:52 +0200

    Added ship movement in both directions, changed ship icon

--- ship.py
+++ ship.py
@@ -8,28 +8,37 @@ class Ship():
         self.screen = screen
         
         # Loading the ship image and its rect
-        self.image = pygame.transform.scale(pygame.image.load('images/ship.bmp'), (60, 80))
+        self.image = pygame.transform.scale(pygame.image.load('images/ship.bmp'), (80, 100))
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # Each new spaceship will show up at the bottom of the screen
+        # Each new spaceship will show up in the center of the screen
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.centery
+        # self.rect.bottom = self.screen_rect.bottom
 
-        self.center = float(self.rect.centerx)
+        self.x_pos = float(self.rect.centerx)
+        self.y_pos = float(self.rect.centery)
 
         # Ship's moving right flag
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         ''' Update the position of the ship on the screen '''
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
+            self.x_pos += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed_factor
+            self.x_pos -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > 0:
+            self.y_pos -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y_pos += self.ai_settings.ship_speed_factor
 
-        self.rect.centerx = self.center
+        self.rect.centerx = self.x_pos
+        self.rect.centery = self.y_pos
 
     def blitme(self):
         ''' Displaying the ship in a current location '''
