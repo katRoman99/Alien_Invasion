393b9522e697038d8883e5183f8c536de7fb1d3c
Kasia Romaniuk <k.romaniuk99@gmail.com>
2020-04-11 21:27:36 +0200

    Fixed a game crash when launching a game

--- game_functions.py
+++ game_functions.py
@@ -69,7 +69,7 @@ def get_number_aliens_x(ai_settings, alien_width):
 
 def get_number_rows(ai_settings, alien_height, ship_height):
     available_space_y = ai_settings.screen_height - 3*alien_height - ship_height
-    number_rows = int(available_space_y/ 2*alien_height)
+    number_rows = int(available_space_y/ (2*alien_height))
     return number_rows
 
 def create_alien(ai_settings, screen, aliens, alien_number, row_number):
